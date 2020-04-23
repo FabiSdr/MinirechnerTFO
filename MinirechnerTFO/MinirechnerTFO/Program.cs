01a577bce2f86761c32b92fc392b85af32148ab6
Fabian Sieder <02siefab@rgtfo-me.it>
2020-04-23 10:33:39 +0200

    1. Commit

---
+++ MinirechnerTFO/MinirechnerTFO/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MinirechnerTFO
+{
+    class Program
+    {
+        //Erstellt von Fabian Sieder am 23.04.2020
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
