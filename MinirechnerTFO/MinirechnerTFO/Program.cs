9bfd501021dfaa55665ba4f98e5bc77031484c97
Fabian Sieder <02siefab@rgtfo-me.it>
2020-04-23 11:07:32 +0200

    Kommentare hinzugefügt

--- MinirechnerTFO/MinirechnerTFO/Program.cs
+++ MinirechnerTFO/MinirechnerTFO/Program.cs
@@ -10,11 +10,12 @@ namespace MinirechnerTFO
         //Erstellt von Fabian Sieder am 23.04.2020
         static void Main(string[] args)
         {
+            //Zahlen einlesen
             Console.Write("Geben Sie Zahl1 ein: ");
             int Zahl1 = Convert.ToInt32(Console.ReadLine());
             Console.Write("Geben Sie Zahl2 ein: ");
             int Zahl2 = Convert.ToInt32(Console.ReadLine());
-
+            
             int ErgAddition = Addiere(Zahl1, Zahl2);
             Console.WriteLine("Zahl1 + Zahl2 = " + ErgAddition);
 
@@ -29,25 +30,25 @@ namespace MinirechnerTFO
 
             Console.ReadKey();
         }
-
+        //Methode zum Addieren der beiden Zahlen
         static int Addiere(int Zahl1, int Zahl2)
         {
             int Erg = Zahl1 + Zahl2;
             return Erg;
         }
-
+        //Methode zum Subtrahieren der beiden Zahlen
         static int Subtrahiere(int Zahl1, int Zahl2)
         {
             int Erg = Zahl1 - Zahl2;
             return Erg;
         }
-
+        //Methode zum Multiplizieren der beiden Zahlen
         static int Multipliziere(int Zahl1, int Zahl2)
         {
             int Erg = Zahl1 * Zahl2;
             return Erg;
         }
-
+        //Methode zum Dividieren der beiden Zahlen
         static int Dividiere(int Zahl1, int Zahl2)
         {
             int Erg = Zahl1 / Zahl2;
